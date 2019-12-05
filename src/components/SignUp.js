5e10af32c8ae8c6d2b4fe4c6fb6f361ae1b68964
dung-phan <madewithcrayons@gmail.com>
2019-12-05 17:55:10 +0100

    finish styling signup and login page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import Form from './Form';
-import { connect } from 'react-redux';
-import { signup } from '../actions/signup';
-import { Link } from 'react-router-dom';
-import NavBar from './NavBar';
+import React, { Component } from "react";
+import Form from "./Form";
+import { connect } from "react-redux";
+import { signup } from "../actions/signup";
+import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 class SignUp extends Component {
-  state = { email: '', password: '' };
+  state = { email: "", password: "" };
 
   handleSubmit = event => {
     event.preventDefault();
     this.props.signup(this.state.email, this.state.password);
-    this.props.history.push('/login');
+    this.props.history.push("/login");
   };
   handleChange = event => {
     this.setState({
@@ -21,9 +21,10 @@ class SignUp extends Component {
     return (
       <div>
         <NavBar />
-        <div className='ui middle aligned center aligned grid'>
-          <div className='column' style={{ maxWidth: 450 }}>
-            <h2 className='content'>Sign up for a new account</h2>
+
+        <div className="ui middle aligned center aligned grid">
+          <div className="column" style={{ maxWidth: 450, paddingTop: "10%" }}>
+            <h2 className="content">Sign up for a new account</h2>
           </div>
         </div>
         <Form
@@ -32,20 +33,26 @@ class SignUp extends Component {
           handleChange={this.handleChange}
         />
 
-        <div className='ui middle aligned center aligned grid'>
-          <div className='column' style={{ maxWidth: 450 }}>
-            <div className='ui message'>
-              <i className='icon help'></i> Already have an account?{' '}
-              <Link to='/login'>Login here</Link>
+        <div className="ui center aligned grid">
+          <div className="column" style={{ maxWidth: 450 }}>
+            <div className="ui message">
+              <p>
+                Already have an account? <Link to="/login">Login here</Link>
+              </p>
             </div>
           </div>
+        </div>
+        <div>
+          <img
+            style={{ width: "30%", transform: "translate(-10%, -90%)" }}
+            className="float-right"
+            src="http://embacy.io/images/04-il.svg"
+            alt=""
+          />
         </div>
       </div>
     );
   }
 }
 
-export default connect(
-  null,
-  { signup }
-)(SignUp);
+export default connect(null, { signup })(SignUp);
