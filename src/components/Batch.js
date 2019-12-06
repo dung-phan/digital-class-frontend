9a43a88bce3dee057309bcc14f2b2983969ee200
dung-phan <madewithcrayons@gmail.com>
2019-12-06 15:03:56 +0100

    fix styling class detail page

--- src/components/Batch.js
+++ src/components/Batch.js
@@ -1,59 +1,77 @@
-import React, { Component } from 'react';
-import { loadBatches } from '../actions/batches';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import CreateNewBatch from './CreateNewBatch';
-import NavBar from './NavBar';
+import React, { Component } from "react";
+import { loadBatches } from "../actions/batches";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import CreateNewBatch from "./CreateNewBatch";
+import NavBar from "./NavBar";
 export class Batch extends Component {
   componentDidMount() {
-    console.log('did component did mount works');
+    console.log("did component did mount works");
     this.props.loadBatches();
   }
   render() {
     return (
-      <div>
+      <div className="containerx">
         <NavBar />
+        <div className="top-heading">Batch details</div>
         {this.props.batches === null ? (
-          'Loading...'
+          "Loading..."
         ) : (
-          <ul className='ui three column grid'>
-            {console.log('check the props', this.props)}
-            {this.props.batches.map(batch => (
-              <li key={batch.id} className='ui card column'>
-                <div className='segment'>
-                  <Link className='header' to={`/batches/${batch.id}/students`}>
-                    Batch #{batch.batchNumber}
-                  </Link>
-                </div>
-                <div className='content'>
-                  <h4 className='ui sub header'>Details</h4>
-                  Start date: {batch.startDate}
-                  <br />
-                  End date: {batch.endDate}
-                </div>
-              </li>
-            ))}
-          </ul>
+          <div>
+            <img
+              src="http://embacy.io/images/v2.svg"
+              alt=""
+              className="float-left"
+            />
+            <ul className="ui three column grid">
+              {console.log("check the props", this.props)}
+
+              {this.props.batches.map(batch => (
+                <li
+                  key={batch.id}
+                  className="ui card column"
+                  style={{
+                    marginLeft: "5%",
+                    marginRight: "5%",
+                    width: "20%",
+                    marginTop: "7%"
+                  }}
+                >
+                  <div className="segment">
+                    <Link
+                      className="header"
+                      to={`/batches/${batch.id}/students`}
+                    >
+                      <h3>Batch #{batch.batchNumber}</h3>
+                    </Link>
+                  </div>
+                  <div className="content">
+                    <h4>Details</h4>
+                    Start date: {batch.startDate}
+                    <br />
+                    End date: {batch.endDate}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         )}
         <br />
 
         {this.props.loggedIn ? (
           <CreateNewBatch />
         ) : (
-          <Link to='/login'>Please log in to create classes</Link>
+          <Link to="/login">Please log in to create classes</Link>
         )}
       </div>
     );
   }
 }
 const mapStateToProps = state => {
-  console.log('check state auth', state.auth);
+  console.log("check state auth", state.auth);
   return {
     batches: state.batches,
     loggedIn: !!state.auth
   };
 };
-export default connect(
-  mapStateToProps,
-  { loadBatches }
-)(Batch);
+export default connect(mapStateToProps, { loadBatches })(Batch);
--- src/components/CreateNewBatch.js
+++ src/components/CreateNewBatch.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { createBatch } from '../actions/batches';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createBatch } from "../actions/batches";
 
 class CreateNewBatch extends Component {
   state = {
-    batchNumber: '',
-    startDate: '',
-    endDate: ''
+    batchNumber: "",
+    startDate: "",
+    endDate: ""
   };
   handleChange = event => {
     //hey it's gotta be arrow function here
@@ -17,57 +17,71 @@ class CreateNewBatch extends Component {
     event.preventDefault();
 
     this.props.createBatch(this.state);
-    console.log('check what is state before setState', this.state);
+    console.log("check what is state before setState", this.state);
     //set the form field to be empty again
     this.setState({
-      batchNumber: '',
-      startDate: '',
-      endDate: ''
+      batchNumber: "",
+      startDate: "",
+      endDate: ""
     });
-    console.log('check what is state after setState', this.state);
+    console.log("check what is state after setState", this.state);
   };
   render() {
     //const isEnabled = this.state.name.length > 0;
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Batch Number:{' '}
-            <input
-              type='text'
-              name='batchNumber'
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </label>{' '}
-          <label>
-            <br />
-            Start Date:{' '}
-            <input
-              type='text'
-              name='startDate'
-              value={this.state.startDate}
-              onChange={this.handleChange}
-            />
-          </label>{' '}
-          <label>
-            <br />
-            End Date:
-            <input
-              type='text'
-              name='endDate'
-              value={this.state.endDate}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type='submit' value='Submit' />
-        </form>
+        <h2>Want to create a new batch?</h2>
+        <div className="ui middle aligned center aligned grid">
+          <div
+            className="column form"
+            style={{ maxWidth: 300, marginBottom: "15px" }}
+          >
+            <form
+              onSubmit={this.handleSubmit}
+              className="ui large form three fields"
+            >
+              <div className="ui stacked segment">
+                <div className="ui field">
+                  <label>
+                    Batch Number
+                    <input
+                      type="text"
+                      name="batchNumber"
+                      value={this.state.name}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+                <div className="field">
+                  <label>
+                    Start Date:
+                    <input
+                      type="date"
+                      name="startDate"
+                      value={this.state.startDate}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+                <div className="field">
+                  <label>
+                    End Date:
+                    <input
+                      type="date"
+                      name="endDate"
+                      value={this.state.endDate}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+                <input type="submit" value="Submit" className="ui button" />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default connect(
-  null,
-  { createBatch }
-)(CreateNewBatch);
+export default connect(null, { createBatch })(CreateNewBatch);
